9a37ccdd95568a62c9fb60318084815fe28ec7fc
Bryan Brenes <bryanbrenes1@gmail.com>
2023-01-05 19:36:07 -0600

    Add adding and subtracting methods between points and vectors

--- src/point.rs
+++ src/point.rs
@@ -1,14 +1,60 @@
+use crate::vector::Vector;
+use std::ops::Add;
+use std::ops::Sub;
+
 #[derive(Debug)]
 pub struct Point {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-    pub w: i32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
 }
 
 impl Point {
-    pub fn new(x: i32, y: i32, z: i32) -> Point {
-        Point { x, y, z, w: 1 }
+    pub fn new(x: f32, y: f32, z: f32) -> Point {
+        Point { x, y, z, w: 1.0 }
+    }
+}
+
+// point + vector
+impl Add<&Vector> for &Point {
+    type Output = Point;
+
+    fn add(self, other: &Vector) -> Self::Output {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: 1.0,
+        }
+    }
+}
+
+// point - point
+impl Sub<&Point> for &Point {
+    type Output = Vector;
+
+    fn sub(self, other: &Point) -> Self::Output {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: 0.0,
+        }
+    }
+}
+
+// point - vector
+impl Sub<&Vector> for &Point {
+    type Output = Point;
+
+    fn sub(self, other: &Vector) -> Self::Output {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: 1.0,
+        }
     }
 }
 
@@ -18,15 +64,15 @@ mod tests {
 
     #[test]
     fn should_have_w_1_by_default() {
-        let point = Point::new(1, 2, 3);
-        assert_eq!(point.w, 1);
+        let point = Point::new(1.0, 2.0, 3.0);
+        assert_eq!(point.w, 1.0);
     }
 
     #[test]
     fn should_be_able_to_create_points() {
-        let point = Point::new(2, -4, 5);
-        assert_eq!(point.x, 2);
-        assert_eq!(point.y, -4);
-        assert_eq!(point.z, 5);
+        let point = Point::new(2.0, -4.0, 5.0);
+        assert_eq!(point.x, 2.0);
+        assert_eq!(point.y, -4.0);
+        assert_eq!(point.z, 5.0);
     }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,60 @@
+use std::ops::Add;
+use std::ops::Sub;
+use crate::point::Point;
+
 #[derive(Debug)]
 pub struct Vector {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-    pub w: i32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
 }
 
 impl Vector {
-    pub fn new(x: i32, y: i32, z: i32) -> Vector {
-        Vector { x, y, z, w: 0 }
+    pub fn new(x: f32, y: f32, z: f32) -> Vector {
+        Vector { x, y, z, w: 0.0 }
+    }
+}
+
+// vector + vector
+impl Add<&Vector> for &Vector {
+    type Output = Vector;
+
+    fn add(self, other: &Vector) -> Self::Output {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: 0.0,
+        }
+    }
+}
+
+// vector + point
+impl Add<&Point> for &Vector {
+    type Output = Point;
+
+    fn add(self, other: &Point) -> Self::Output {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: 1.0,
+        }
+    }
+}
+
+// vector - vector
+impl Sub<&Vector> for &Vector {
+    type Output = Vector;
+
+    fn sub(self, other: &Vector) -> Self::Output {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: 0.0,
+        }
     }
 }
 
@@ -18,15 +64,15 @@ mod tests {
 
     #[test]
     fn should_have_w_0_by_default() {
-        let vector = Vector::new(1,2,3);
-        assert_eq!(vector.w, 0);
+        let vector = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(vector.w, 0.0);
     }
 
     #[test]
     fn should_be_able_to_create_vectors() {
-        let vector = Vector::new(2, -4, 5);
-        assert_eq!(vector.x, 2);
-        assert_eq!(vector.y, -4);
-        assert_eq!(vector.z, 5);
+        let vector = Vector::new(2.0, -4.0, 5.0);
+        assert_eq!(vector.x, 2.0);
+        assert_eq!(vector.y, -4.0);
+        assert_eq!(vector.z, 5.0);
     }
 }
