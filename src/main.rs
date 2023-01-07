81734f458d5cf64690e48921bb2ab724fb85a0b2
Bryan Brenes <bryanbrenes1@gmail.com>
2023-01-06 19:55:08 -0600

    Paint the projectile trajectory

--- src/main.rs
+++ src/main.rs
@@ -20,28 +20,45 @@ struct Env {
 }
 
 fn main() {
-    let mut canvas = Canvas::new(100, 100);
-    canvas.write_pixel(&Color::new(1.0, 0.5, 0.3), 0, 0);
-    canvas.write_pixel(&Color::new(1.0, 0.5, 0.3), 100, 100);
-    canvas.to_ppm();
+    let mut canvas = Canvas::new(350, 350);
 
     let mut projectile = Projectile {
-        position: Point::new(0.0, 1.0, 0.0),
-        velocity: Vector::new(1.0, 1.0, 0.0),
+        position: Point::new(0.0, 0.0, 0.0),
+        velocity: Vector::new(0.7, 1.8, 0.0),
     };
     let env = Env {
-        gravity: Vector::new(0.0, -0.1, 0.0),
+        gravity: Vector::new(0.0, -0.09, 0.0),
         wind: Vector::new(-0.01, 0.0, 0.0),
     };
 
-    while projectile.position.y > 0.0 {
+    while projectile.position.y >= 0.0 {
         projectile = tick(&env, &projectile);
         println!("Current position {:?}", projectile.position);
+        paint_square(
+            &mut canvas,
+            (projectile.position.x * 15.0) as usize,
+            350 - (projectile.position.y * 15.0) as usize,
+            2,
+        )
     }
+    canvas.to_ppm();
 }
 
 fn tick(env: &Env, proj: &Projectile) -> Projectile {
     let position = &proj.position + &proj.velocity;
     let velocity = &(&proj.velocity + &env.gravity) + &env.wind;
     Projectile { position, velocity }
+}
+
+fn paint_square(canvas: &mut Canvas, x: usize, y: usize, size: usize) {
+    let initial_pos_x = (x - size).clamp(0, 350);
+    let initial_pos_y = (y - size).clamp(0, 350);
+    let final_pos_x = initial_pos_x + (2 * size + 1);
+    let final_pos_y = initial_pos_y + (2 * size + 1);
+
+    for row_index in initial_pos_y..final_pos_y {
+        for col_index in initial_pos_x..final_pos_x {
+            canvas.write_pixel(&Color::new(1.0, 0.0, 0.0), col_index, row_index);
+        }
+    }
 }
